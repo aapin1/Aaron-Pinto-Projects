936b71dea7ff7aa8b616e8f27856847a4e6a5c1b
aapin1 <119368499+aapin1@users.noreply.github.com>
2024-02-08 08:43:40 -0500

    Update main.py

--- Parallel Line Detection With Centerline/main.py
+++ Parallel Line Detection With Centerline/main.py
@@ -9,10 +9,12 @@ while True:
     mask = np.zeros(frame.shape[:2], np.uint8)
     mask[100:1000, 300:850] = 255
 
+    rect = cv2.rectangle(frame, (600,700),(800,1100),(255,80,10),3)
+
     # compute the bitwise AND using the mask
     masked_img = cv2.bitwise_and(frame, frame, mask=mask)
 
-    gray = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY).
+    gray = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY)
 
     edges = cv2.Canny(gray, 50, 150, apertureSize = 3)
 
@@ -24,7 +26,8 @@ while True:
         cv2.line(masked_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
         lines_list.append([(x1, y1), (x2, y2)])
 
-    cv2.imshow('Line Detection', masked_img)
+        #use masked image variable for line detection
+    cv2.imshow('Line Detection', frame)
 
 
     if cv2.waitKey(1) == 27:
@@ -33,6 +36,16 @@ while True:
 #make mask follow around the paper
 #use line detectipon in masked image
 
+
+
+
+
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+
+
+
+
+
+
