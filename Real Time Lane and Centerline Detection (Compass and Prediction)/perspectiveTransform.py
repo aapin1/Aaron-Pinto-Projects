908b01be6be9798cbd767d54e011aa0df6732fbb
aapin1 <119368499+aapin1@users.noreply.github.com>
2024-03-25 15:34:35 -0400

    Update perspectiveTransform.py

--- Real Time Lane and Centerline Detection (Compass and Prediction)/perspectiveTransform.py
+++ Real Time Lane and Centerline Detection (Compass and Prediction)/perspectiveTransform.py
@@ -11,17 +11,25 @@ while True:
 
     # Locate points of the documentsy
     # or object which you want to transform
-    pts1 = np.float32([[-1050, 860], [-430, 860],
-                       [-1050, 1000], [-430, 1000]])
-    pts2 = np.float32([[-1050, 600], [-650, 600],
-                       [-1050, 1240], [-650, 1240]])
+    pts1 = np.float32([[-1050, 960], [-430, 960],
+                       [-1050, 1100], [-430, 1100]])
+    pts2 = np.float32([[-1050, 700], [-650, 700],
+                       [-1050, 1340], [-650, 1340]])
 
     # Apply Perspective Transform Algorithm
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     result = cv2.warpPerspective(frame, matrix, (500, 600))
 
+    # frame is 1080 by 1920
+    cropped_image = frame[0:900, 0:1920]
+
+    #height, width = frame.shape[:2]
+    #print(f"height: {height}")
+    #print(f"width: {width}")
+
+
     # Wrap the transformed image
-    cv2.imshow('frame', frame)  # Initial Capture
+    cv2.imshow('frame', cropped_image)  # Initial Capture
     cv2.imshow('frame1', result)  # Transformed Capture
 
     if cv2.waitKey(24) == 27:
